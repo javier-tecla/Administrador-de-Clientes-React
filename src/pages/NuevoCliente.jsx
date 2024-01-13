67b2bd8a3ac9e1d6caf46a8fa5434a810f90697d
javierborjas <cristman11@gmail.com>
2024-01-12 23:13:17 -0300

    Conectando JSON-Server a nuestro Proyecto

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -4,14 +4,21 @@ import Error from "../components/Error"
 
 export async function action({request}) {
    const formData = await request.formData()
-
    const datos = Object.fromEntries(formData)
+   const email = formData.get('email')
 
    // Validación
    const errores = []
    if(Object.values(datos).includes('')) {
       errores.push('Todos los campos son obligatorios')
    }
+
+   let regex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
+   if(!regex.test(email)) {
+      errores.push('El Email no es válido')
+   }
+
+
 
    // Retornar datos si hay errores
    if(Object.keys(errores).length) {
@@ -50,6 +57,7 @@ function NuevoCliente() {
 
           <Form
               method="POST"
+              noValidate
               
           >
               <Formulario />
